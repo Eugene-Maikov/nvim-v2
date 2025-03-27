53bbf6ba9150904b27353ef68ea0508c0071d135
Eugene Maikov <61806051+Eugene-Maikov@users.noreply.github.com>
2025-03-27 11:20:13 +0300

    del: replicate auto-rename-tag

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,8 +47,6 @@ return {
       },
     },
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -95,28 +93,6 @@ return {
       "nvim-treesitter/nvim-treesitter-textobjects",
       "RRethy/nvim-treesitter-textsubjects",
     },
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    event = "BufReadPre",
-    config = function()
-      require('nvim-ts-autotag').setup({
-        opts = {
-          enable_close = false,          -- Auto close tags
-          enable_rename = true,          -- Auto rename pairs of tags
-          enable_close_on_slash = true   -- Auto close on trailing </
-        },
-        -- Also override individual filetype configs, these take priority.
-        -- Empty by default, useful if one of the "opts" global settings
-        -- doesn't work well in a specific filetype
-        --[[ per_filetype = {
-            ["html"] = {
-              enable_close = false
-            }
-          } ]]
-      })
-    end
-
   },
   {
     "wurli/contextindent.nvim",
